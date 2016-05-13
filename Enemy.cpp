30e60c529d008f14fc98b1256b1dc56f33be5dfc
schlenkibus <justus.scheil@parteimail.de>
2016-05-13 14:41:15 +0200

    Refractored Enemy Class

---
+++ Enemy.cpp
@@ -0,0 +1,78 @@
+#include "Enemy.hpp"
+
+Book::Book(sf::Vector2f pos)
+{
+  srand((unsigned)time(0));
+  x = pos.x;
+  y = pos.y;
+  sprite.setPosition(pos);
+
+  spriteSheetL.loadFromFile("art/mobs/books/bookL.png");
+  spriteSheetR.loadFromFile("art/mobs/books/bookR.png");
+
+  idleL.setSpriteSheet(spriteSheetL);
+  idleR.setSpriteSheet(spriteSheetR);
+
+  idleL.addFrame(sf::IntRect(7, 21, 93, 55));
+  idleL.addFrame(sf::IntRect(114, 21, 69, 59));
+  idleL.addFrame(sf::IntRect(203, 20, 52, 57));
+
+  idleR.addFrame(sf::IntRect(5, 22, 93, 57));
+  idleR.addFrame(sf::IntRect(111, 20, 69, 59));
+  idleR.addFrame(sf::IntRect(196, 21, 52, 56));
+
+  sprite.setFrameTime(sf::seconds(0.2f));
+
+  sprite.setAnimation(idleR);
+
+  waypoint = generateNewWaypoint();
+  linearInterpolation();
+}
+
+void Book::update(sf::Time deltaTime)
+{
+  if(dir.x > 0 && faceRight == false)
+  {
+    faceRight = true;
+    sprite.setAnimation(idleR);
+  }
+  if(faceRight == true && dir.x < 0)
+  {
+    faceRight = false;
+    sprite.setAnimation(idleL);
+  }
+  sprite.update(deltaTime);
+  pathfinding();
+}
+
+void Book::pathfinding()
+{
+  if( sprite.getPosition().x >= waypoint.x - 20 &&
+      sprite.getPosition().x <= waypoint.x + 20 &&
+      sprite.getPosition().y >= waypoint.y - 20 &&
+      sprite.getPosition().y <= waypoint.y + 20)
+  {
+    waypoint = generateNewWaypoint();
+    linearInterpolation();
+  }
+  else
+  {
+    sprite.move(dir * 0.003f);
+  }
+}
+
+sf::Vector2f Book::generateNewWaypoint()
+{
+  return sf::Vector2f(RandomFloat(50, 900), RandomFloat(0, 600));
+}
+
+void Book::linearInterpolation()
+{
+  dir = normalize(waypoint, sprite.getPosition());
+}
+
+float Book::RandomFloat(float min, float max)
+{
+  float r = (float)rand() / (float)RAND_MAX;
+  return min + r * (max - min);
+}
--- Enemy.hpp
+++ Enemy.hpp
@@ -40,77 +40,12 @@ protected:
 class Book : public Enemy
 {
 public:
-  Book(sf::Vector2f pos)
-  {
-    srand((unsigned)time(0));
-    x = pos.x;
-    y = pos.y;
-    sprite.setPosition(pos);
-
-    spriteSheetL.loadFromFile("art/mobs/books/bookL.png");
-    spriteSheetR.loadFromFile("art/mobs/books/bookR.png");
-
-    idleL.setSpriteSheet(spriteSheetL);
-    idleR.setSpriteSheet(spriteSheetR);
-
-    idleL.addFrame(sf::IntRect(7, 21, 93, 55));
-    idleL.addFrame(sf::IntRect(114, 21, 69, 59));
-    idleL.addFrame(sf::IntRect(203, 20, 52, 57));
-
-    idleR.addFrame(sf::IntRect(5, 22, 93, 57));
-    idleR.addFrame(sf::IntRect(111, 20, 69, 59));
-    idleR.addFrame(sf::IntRect(196, 21, 52, 56));
-
-    sprite.setFrameTime(sf::seconds(0.2f));
-
-    sprite.setAnimation(idleR);
-
-    waypoint = generateNewWaypoint();
-    linearInterpolation();
-  };
-  void update(sf::Time deltaTime)
-  {
-    if(dir.x > 0 && faceRight == false)
-    {
-      faceRight = true;
-      sprite.setAnimation(idleR);
-    }
-    if(faceRight == true && dir.x < 0)
-    {
-      faceRight = false;
-      sprite.setAnimation(idleL);
-    }
-    sprite.update(deltaTime);
-    pathfinding();
-  };
-  void pathfinding()
-  {
-    if( sprite.getPosition().x >= waypoint.x - 20 &&
-        sprite.getPosition().x <= waypoint.x + 20 &&
-        sprite.getPosition().y >= waypoint.y - 20 &&
-        sprite.getPosition().y <= waypoint.y + 20)
-    {
-      waypoint = generateNewWaypoint();
-      linearInterpolation();
-    }
-    else
-    {
-      sprite.move(dir * 0.003f);
-    }
-  };
-  sf::Vector2f generateNewWaypoint()
-  {
-    return sf::Vector2f(RandomFloat(50, 900), RandomFloat(0, 600));
-  };
-  void linearInterpolation()
-  {
-    dir = normalize(waypoint, sprite.getPosition());
-  }
-  float RandomFloat(float min, float max)
-  {
-	  float r = (float)rand() / (float)RAND_MAX;
-	  return min + r * (max - min);
-  }
+  Book(sf::Vector2f pos);
+  void update(sf::Time deltaTime);
+  void pathfinding();
+  sf::Vector2f generateNewWaypoint();
+  void linearInterpolation();
+  float RandomFloat(float min, float max);
 private:
   float x, y, speed, precision;
   sf::Vector2f dir;
